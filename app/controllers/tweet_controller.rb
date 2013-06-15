bb52c43f5ee25a749b5f3e83c5ffdd697f58f2f2
Dan Silver <das2c3@mail.missouri.edu>
2013-06-15 12:42:41 -0500

    workers remote line

---
+++ app/controllers/tweet_controller.rb
@@ -0,0 +1,4 @@
+class TweetController < ApplicationController
+  def processed
+  end
+end
--- workers/getTweets.rb
+++ workers/getTweets.rb
@@ -7,8 +7,8 @@ TweetStream.configure do |config|
   config.oauth_token_secret = "656QqQgMJKaNcRju5roHCJKpqo4kDIcMp4Gn0kw0uE"
   config.auth_method        = :oauth
 end
-ironmq = IronMQ::Client.new :token => "f29MgpP0JbVlnDJb0ii7Cmzkwg8", :project_id => "51bc92fd2267d85283001145"
-queue = ironmq.queue "tweets"
+#ironmq = IronMQ::Client.new :token => "f29MgpP0JbVlnDJb0ii7Cmzkwg8", :project_id => "51bc92fd2267d85283001145"
+#queue = ironmq.queue "tweets"
 @client = TweetStream::Client.new
 @client.track("obama", "potus") do |status|
 #  queue.post status.text
