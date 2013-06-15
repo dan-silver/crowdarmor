f98b8b32633a2deafda8dd47cca6ee687111e8a4
Ryan Endacott <rzeg24@gmail.com>
2013-06-15 18:35:36 -0500

    finished primary

--- app/controllers/tweet_controller.rb
+++ app/controllers/tweet_controller.rb
@@ -26,7 +26,7 @@ class TweetController < ApplicationController
     if not current_user
       redirect_to "/"
     else
-      @tweets = current_user.tweets
+      @tweets = current_user.tweets.where(:tweet_type => 'primary')
       render "tweet/index"
     end
   end
@@ -34,13 +34,15 @@ class TweetController < ApplicationController
 
   private
   def save_original_tweet(tweet)
-    unless Tweet.where(:tweet_id => tweet.tweet_id).count > 0
+    if Tweet.where(:tweet_id => tweet.tweet_id, :tweet_type => 'primary').count == 0
+      puts 'Determining primary tweet...'
       primary = Tweet.new
       primary.tweet_id = tweet.tweet_id
       primary.score = tweet.score # aggregates starts as one reply score
       primary.user = tweet.user
       primary.type = 'primary'
       primary.body = Twitter.status(tweet.tweet_id).text #twitter gem here
+      primary.save
     end
   end
 end
