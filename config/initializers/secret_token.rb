f93d597a71fc1cf9e6b6f43d55f5898b84561126
Dan Silver <das2c3@mail.missouri.edu>
2013-06-15 10:37:47 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Crowdarmor::Application.config.secret_token = '4fa20a7e52321853543263467fc0e0741dd66dcc1ef32aec8e43f45d37817cc0ff6db44bc1cbbc7c518bf1c59705d2dd54ed30b8dadd1c9a2a9bb67622688c89'
