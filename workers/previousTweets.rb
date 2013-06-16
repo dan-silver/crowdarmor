85e39f86ee5b395c4990583e012d60af9d8446c0
Dan Silver <das2c3@mail.missouri.edu>
2013-06-16 01:40:44 -0500

    worker updates

--- workers/previousTweets.rb
+++ workers/previousTweets.rb
@@ -1,9 +1,19 @@
 require "iron_mq"
 require "twitter"
 
+Twitter.configure do |config|
+  config.consumer_key = "d97mHG7MRvESV0cIs2gMbg"
+  config.consumer_secret = "V1apx1fjO0ynLYZzcW9s3xff3l0ifI3kTmZqV59zng"
+  config.oauth_token = "1519626841-AOn8G3h9NyxsEQELHmIGW9phit1J7Ph2oXRZaQT"
+  config.oauth_token_secret = "656QqQgMJKaNcRju5roHCJKpqo4kDIcMp4Gn0kw0uE"
+end
+
 puts "Getting previous tweets..."
+params ={"Token"=>"1519626841-AOn8G3h9NyxsEQELHmIGW9phit1J7Ph2oXRZaQT", "TokenSecret"=>"656QqQgMJKaNcRju5roHCJKpqo4kDIcMp4Gn0kw0uE", "Twitter_Handle"=>"crowdarmor"}
+puts params
 ironmq = IronMQ::Client.new :token => "f29MgpP0JbVlnDJb0ii7Cmzkwg8", :project_id => "51bc92fd2267d85283001145"
 @@queue = ironmq.queue "tweets"
+
 
 @client = Twitter::Client.new(
   :oauth_token => params['Token'],
