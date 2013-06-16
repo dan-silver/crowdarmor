db67a7c8c7c6996c097c7e441d0f689170b8f3b5
Dan Silver <das2c3@mail.missouri.edu>
2013-06-15 21:16:13 -0500

    Analysis worker created

--- lib/worker_launcher.rb
+++ lib/worker_launcher.rb
@@ -4,14 +4,27 @@ class WorkerLauncher
 
   @@client = IronWorkerNG::Client.new
   @@current_task = false
+  @@current_analysis = false
 
   # Launches the tweet crawler and removes all previous instances
   # Used to refresh the list of twitter usernames being crawled
+
   def self.launch_tweet_crawler
     @@client.tasks.cancel(@@current_task.id) if @@current_task
     handles = User.select(:Twitter_Handle).collect(&:Twitter_Handle)
     puts 'Launching tweet crawler with handles:'
     puts handles
     @@current_task = @@client.tasks.create("getTweets", :handles => handles)
+  end
+
+  def self.launch_analysis_worker
+    @@client.tasks.cancel(@@current_analysis.id) if @@current_analysis
+    puts "Launching analysis worker"
+    @@current_analysis = @@client.tasks.create("analysis_worker")
+  end
+
+  def self.launch_workers
+    self.launch_analysis_worker
+    self.launch_tweet_crawler
   end
 end
